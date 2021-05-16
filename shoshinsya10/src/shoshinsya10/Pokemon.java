669bd4b3769eee26cd9db5a8a7ca09cee91f45ef
kzk-hrd <leo.k20504@gmail.com>
2021-05-16 23:42:18 +0900

    課題10提出

---
+++ shoshinsya10/src/shoshinsya10/Pokemon.java
@@ -0,0 +1,15 @@
+package shoshinsya10;
+
+public class Pokemon {
+
+	private int hp = 100;
+
+	public void attack() {
+		System.out.println("進化前のポケモンの攻撃です。");
+	}
+
+	public void deffence() {
+		System.out.println("進化前のポケモンの防御です");
+	}
+
+}
