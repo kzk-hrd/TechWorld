52c0a50f14bdb4551db999fdad19a3164b309743
kzk-hrd <leo.k20504@gmail.com>
2021-05-27 22:24:45 +0900

    第11回講義課題1

--- shoshinsya9/src/shoshinsya9/Magician.java
+++ shoshinsya9/src/shoshinsya9/Magician.java
@@ -2,64 +2,14 @@ package shoshinsya9;
 
 import java.util.List;
 
-public class Magician {
+public class Magician extends Charactor {
 
 	// フィールド
-	private String name;         // 名前
-	private int hp;                  // HP
-	private int at;                   // 攻撃力
 	private int mp;                 // MP
-	private int df;                   // 防御力
 	private int calMp = 20;     // 魔法攻撃消費MP
 	private int magicAt = 20; //  魔法攻撃力
 
 	// メソッド
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		if(name.equals("")) {
-			throw new IllegalArgumentException
-			("名前がNULLです");
-		} else if(name.length() < 3 || name.length() > 10) {
-			throw new IllegalArgumentException
-			("名前の設定は3文字以上、10文字以下で設定してください");
-		}
-		this.name = name;
-	}
-	public int getHp() {
-		return hp;
-	}
-	public void setHp(int hp) {
-		if(hp < 0) {
-			this.hp = 0;
-		} else {
-			this.hp = hp;
-		}
-
-	}
-	public int getAt() {
-		return at;
-	}
-	public void setAt(int at) {
-		if(at < 0) {
-			this.at = 0;
-		} else {
-			this.at = at;
-		}
-
-	}
-	public int getDf() {
-		return df;
-	}
-	public void setDf(int df) {
-		if(df < 0) {
-			this.df = 0;
-		} else {
-			this.df = df;
-		}
-
-	}
 	public int getMp() {
 		return mp;
 	}
@@ -73,9 +23,9 @@ public class Magician {
 	}
 
 	public void attack(Monster m) {
-		System.out.println(this.name + "の攻撃!");
-		System.out.println("敵にダメージを" + this.at + "与えた！");
-		m.setHp(m.getHp() - this.at);
+		System.out.println(super.getName() + "の攻撃!");
+		System.out.println("敵にダメージを" + super.getAt() + "与えた！");
+		m.setHp(m.getHp() - super.getAt());
 		if(m.getHp() <= 0) {
 			System.out.println(m.getName() + "のhpは0以下になりました、負けです。");
 		}
@@ -84,11 +34,11 @@ public class Magician {
 
 	public void magicAttack(List<Monster> monsterList) {
 
-		System.out.println(this.name + "の魔法攻撃!");
+		System.out.println(super.getName() + "の魔法攻撃!");
 		System.out.println("MPを"+calMp +"消費する");
 
 		if(this.mp < calMp) {
-			System.out.println("MPが不足しているため" + this.name + "は魔法攻撃を使用できません。");
+			System.out.println("MPが不足しているため" + super.getName() + "は魔法攻撃を使用できません。");
 			healMagic();
 			System.out.println();
 		} else {
@@ -102,13 +52,20 @@ public class Magician {
 	}
 
 	public void healMagic() {
-		System.out.println( this.name+"が回復した");
-		this.hp = this.hp + 10;
+		System.out.println( super.getName()+"が回復した");
+		super.setHp(super.getHp() + 10);
 		this.mp = this.mp + 20;
-		System.out.println("hpが10回復し、" + this.hp + "になった");
+		System.out.println("hpが10回復し、" + super.getHp() + "になった");
 		System.out.println("mpが20回復し、" + this.mp + "になった");
 		System.out.println();
 	}
+
+	public void deffence() {
+		System.out.println(super.getName() +"が防御した");
+		super.setDf(super.getDf() + 10);
+		System.out.println("防御力が10upし、" + super.getDf() + "になった");
+	}
+
 	public int getCalMp() {
 		return calMp;
 	}
--- shoshinsya9/src/shoshinsya9/Monster.java
+++ shoshinsya9/src/shoshinsya9/Monster.java
@@ -1,79 +1,26 @@
 package shoshinsya9;
 
-public class Monster {
-
-	// フィールド
-	private String name;         // 名前
-	private int hp;                  // HP
-	private int at;                   // 攻撃力
-	private int df;                   // 防御力
+public class Monster extends Creature {
 
 	// メソッド
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		if(name.equals("")) {
-			throw new IllegalArgumentException
-			("名前がNULLです");
-		} else if(name.length() < 3 || name.length() > 10) {
-			throw new IllegalArgumentException
-			("名前の設定は3文字以上、10文字以下で設定してください");
-		}
-		this.name = name;
-	}
-	public int getHp() {
-		return hp;
-	}
-	public void setHp(int hp) {
-		if(hp < 0) {
-			this.hp = 0;
-		} else {
-			this.hp = hp;
-		}
-
-	}
-	public int getAt() {
-		return at;
-	}
-	public void setAt(int at) {
-		if(at < 0) {
-			this.at = 0;
-		} else {
-			this.at = at;
-		}
-
-	}
-	public int getDf() {
-		return df;
-	}
-	public void setDf(int df) {
-		if(df < 0) {
-			this.df = 0;
-		} else {
-			this.df = df;
-		}
-
-	}
-
-	void attackHero(Hero h, Magician ma) {
-		System.out.println(this.name + "の" + h.getName() + "への攻撃！");
-		System.out.println(h.getName() + "に" + this.at + "のダメージを与えた！");
-		h.setHp(h.getHp() - this.at);
+	public void attackHero(Hero h, Magician ma) {
+		System.out.println(super.getName() + "の" + h.getName() + "への攻撃！");
+		System.out.println(h.getName() + "に" + super.getAt() + "のダメージを与えた！");
+		h.setHp(h.getHp() - super.getAt());
 		System.out.println();
 	}
 
-	void attackMagician(Magician ma, Hero h) {
-		System.out.println(this.name + "の" + ma.getName() + "への攻撃！");
-		System.out.println(this.at + "のダメージ");
-		ma.setHp(ma.getHp() - this.at);
+	public void attackMagician(Magician ma, Hero h) {
+		System.out.println(super.getName() + "の" + ma.getName() + "への攻撃！");
+		System.out.println(super.getAt() + "のダメージ");
+		ma.setHp(ma.getHp() - super.getAt());
 		System.out.println();
 
 	}
 
-	void defence() {
-		System.out.println(this.name + "は守りを固めた");
-		this.df = this.df + 10;
+	public void deffence() {
+		System.out.println(super.getName() + "は守りを固めた");
+		super.setDf(super.getHp() + 10);
 		System.out.println("防御力が10上がった");
 	}
 }
